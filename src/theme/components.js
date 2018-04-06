cb84aeb1cdad5b972b9f26ad1b0eee8d72ed1bc2
Dino Mandic <dinomandic@hotmail.com>
2018-04-05 23:20:16 -0500

    feat: create shared styled components

---
+++ src/theme/components.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #FEC731;
+  padding: 1rem 2.4rem;
+  color: black;
+  font-size: 1rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  border: 0
+  border-radius: 25px;
+  cursor: pointer;
+`;
